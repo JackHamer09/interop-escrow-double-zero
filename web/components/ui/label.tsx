2515da536ab5aba707208f7e336842c37475c5f8
Jack Hamer <jackhamer0990@gmail.com>
2025-05-16 23:14:43 +0300

    Lint fix

--- web/components/ui/label.tsx
+++ web/components/ui/label.tsx
@@ -3,21 +3,19 @@
 import React, { forwardRef } from "react";
 import { cn } from "~~/utils/cn";
 
-export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+export type LabelProps = React.LabelHTMLAttributes<HTMLLabelElement>;
 
-export const Label = forwardRef<HTMLLabelElement, LabelProps>(
-  ({ className, ...props }, ref) => {
-    return (
-      <label
-        ref={ref}
-        className={cn(
-          "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-          className
-        )}
-        {...props}
-      />
-    );
-  }
-);
+export const Label = forwardRef<HTMLLabelElement, LabelProps>(({ className, ...props }, ref) => {
+  return (
+    <label
+      ref={ref}
+      className={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className,
+      )}
+      {...props}
+    />
+  );
+});
 
 Label.displayName = "Label";
